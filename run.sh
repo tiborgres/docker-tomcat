017fd7e610282bc0f430ac6572316d3c6af61013
tiborgres <tibor.gres@gmail.com>
2017-09-29 14:08:47 +0100

    fixed missing folder 'logs'

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 # prepare conf and logs folders as volumes for tomcat container
 rm -rf /opt/tomcat/conf /opt/tomcat/logs
-mkdir -p /opt/tomcat
-cp -R conf logs /opt/tomcat
+mkdir -p /opt/tomcat/logs
+cp -R conf /opt/tomcat
 
 # build image and run the tomcat container
 docker-compose up
